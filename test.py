d1c955c5b80b2e1953368eaf83a8dccb72435d4e
Francisco <franclarke00@hotmail.com>
2023-03-03 16:47:33 -0300

    primer

---
+++ test.py
@@ -0,0 +1,11 @@
+
+def area_tri(b, h):
+    area = (b * h) / 2
+    return area
+
+def suma(a, b):
+    resultado = a + b
+    return resultado
+
+print("El área del triángulo es:", area_tri(10, 5))
+print("La suma de 3 y 5 es:", suma(3, 5))
